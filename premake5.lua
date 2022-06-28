31e0ec47d732c76fa2cbd6db1f9aa613f359e757
bewflast <68069182+bewflast@users.noreply.github.com>
2022-06-29 01:17:57 +0300

    86 и 86_64 инклюды

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+PROJECT_GENERATOR_VERSION = 3
+
+newoption({
+	trigger = "gmcommon",
+	description = "Sets the path to the garrysmod_common (https://github.com/danielga/garrysmod_common) directory",
+	value = "../garrysmod_common"
+})
+
+local gmcommon = assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
+	"you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
+include(gmcommon)
+
+CreateWorkspace({name = "carefree_project_rpc"})
+	CreateProject({serverside = false})
+		IncludeLuaShared()
+		IncludeHelpersExtended()
+		IncludeSDKCommon()
+		IncludeSDKTier0()
+		IncludeSDKTier1()
+		IncludeSteamAPI()
+		IncludeDetouring()
+		IncludeScanning()
+
+		links("discord-rpc")
+		includedirs("discord-rpc/include")
+
+		filter({"platforms:x86_64"})
+			libdirs({"ds_build32/src/Release"})
+		filter({"platforms:x86"})
+			libdirs({"ds_build64/src/Release"})
