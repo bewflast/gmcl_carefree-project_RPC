bc5050af4603d19d81b3b223e2ed4fe350953b7a
bewflast <68069182+bewflast@users.noreply.github.com>
2022-06-29 22:21:48 +0300

    abi compatible

--- premake5.lua
+++ premake5.lua
@@ -10,7 +10,7 @@ local gmcommon = assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
 	"you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
 include(gmcommon)
 
-CreateWorkspace({name = "carefree_project_rpc"})
+CreateWorkspace({name = "carefree_project_rpc", abi_compatible = true})
 	CreateProject({serverside = false})
 		IncludeLuaShared()
 		IncludeHelpersExtended()
