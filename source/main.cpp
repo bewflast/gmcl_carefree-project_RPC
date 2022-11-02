f25bebc0e541bcedb9f87bd1fe1a666629014ba1
Kostya <68069182+bewflast@users.noreply.github.com>
2022-11-02 23:09:30 +0300

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -12,11 +12,11 @@
 namespace globals
 {
 
-	static GarrysMod::Lua::ILuaBase*	Lua				=	nullptr;
-	static IVEngineClient*				engine_client	=	nullptr;
-	static server*						s_info			=	nullptr;
-	static DRPC*						discord			=	nullptr;
-	static ConVar*						host_name		=	nullptr;
+	static GarrysMod::Lua::ILuaBase* Lua = nullptr;
+	static IVEngineClient* engine_client = nullptr;
+	static server* s_info = nullptr;
+	static DRPC* discord = nullptr;
+	static ConVar* host_name = nullptr;
 
 }
 
@@ -37,10 +37,11 @@ int	GetPlayersCount()
 }
 
 
-LUA_FUNCTION( UpdateRPC )
+LUA_FUNCTION(UpdateRPC)
 {
 
 	globals::discord->server_info->playersCount = GetPlayersCount();
+	globals::discord->server_info->serverName = globals::host_name->GetString();
 	globals::discord->update();
 
 	return (0);
@@ -51,18 +52,18 @@ LUA_FUNCTION( UpdateRPC )
 GMOD_MODULE_OPEN()
 {
 
-	globals::Lua			=	LUA;
-	globals::engine_client	=	InterfacePointers::VEngineClient();
-	globals::host_name		=	InterfacePointers::Cvar()->FindVar("hostname");
-	globals::s_info			=	new server(globals::engine_client->GetMaxClients(), globals::host_name->GetString(), globals::engine_client->GetNetChannelInfo()->GetAddress(), GetPlayersCount());
-	globals::discord		=	new DRPC("990599027735281764", globals::s_info);
-	
+	globals::Lua = LUA;
+	globals::engine_client = InterfacePointers::VEngineClient();
+	globals::host_name = InterfacePointers::Cvar()->FindVar("hostname");
+	globals::s_info = new server(globals::engine_client->GetMaxClients(), globals::host_name->GetString(), globals::engine_client->GetNetChannelInfo()->GetAddress(), GetPlayersCount());
+	globals::discord = new DRPC("990599027735281764", globals::s_info);
+
 	globals::discord->update();
 
 	globals::Lua->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB);
 
-		globals::Lua->PushCFunction(UpdateRPC);
-		globals::Lua->SetField(-2, "RPC_Update");
+	globals::Lua->PushCFunction(UpdateRPC);
+	globals::Lua->SetField(-2, "RPC_Update");
 
 	globals::Lua->Pop();
 
@@ -77,8 +78,8 @@ GMOD_MODULE_CLOSE()
 	Discord_Shutdown();
 	delete globals::discord;
 	delete globals::s_info;
-	globals::discord	=	nullptr;
-	globals::s_info		=	nullptr;
+	globals::discord = nullptr;
+	globals::s_info = nullptr;
 
 
 	return (0);
